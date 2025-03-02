3468b1879b71777ce8df3d574717c493a294e1ce
Luc3756 <mluc020203@gmail.com>
2025-03-02 16:42:49 +0100

    fixture

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -11,11 +11,11 @@ class ProductFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $products = [
-            ['name' => 'Ordinateur Portable', 'description' => 'Un ordinateur performant pour le travail et le jeu.', 'price' => 999.99],
-            ['name' => 'Smartphone', 'description' => 'Un smartphone avec un écran OLED et une caméra 108MP.', 'price' => 699.99],
-            ['name' => 'Casque Audio', 'description' => 'Un casque sans fil avec réduction de bruit.', 'price' => 199.99],
-            ['name' => 'Montre Connectée', 'description' => 'Suivi de la santé et notifications en temps réel.', 'price' => 249.99],
-            ['name' => 'Clavier Mécanique', 'description' => 'Un clavier RGB avec switches ultra-réactifs.', 'price' => 129.99],
+            ['name' => 'Poule pondeuse', 'description' => 'Poule pondeuse productive', 'price' => 14.50],
+            ['name' => 'Tracteur Porsche', 'description' => 'Tracteur agricole robuste et fiable de la série Porsche', 'price' => 33000.00],
+            ['name' => 'Poules Araucana', 'description' => 'Poule Araucana célèbre pour ses œufs bleu-vert uniques.', 'price' => 25.10],
+            ['name' => 'Oie cendrée', 'description' => 'Oie cendrée rustique, parfaite pour l’élevage et l’entretien des espaces.', 'price' => 45],
+            ['name' => 'Canard colvert', 'description' => 'Le Canard colvert, col-vert, ou Canard malard au Canada, est une espèce d’oiseaux de l’ordre des Ansériformes, de la famille des Anatidés et de la sous-famille des Anatinés.', 'price' => 30],
         ];
 
         foreach ($products as $data) {
